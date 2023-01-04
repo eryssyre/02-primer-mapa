a61e8a404cdedf1bf8a44b789fa1a3ffa19d1167
eryssyre <121981367+eryssyre@users.noreply.github.com>
2023-01-04 19:33:31 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+import {Map, tileLayer} from 'leaflet'
+
+const mapita = new Map('map').setView([51.505, -0.09], 13);
+
+//url y attributon
+
+tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
+}).addTo(mapita);
